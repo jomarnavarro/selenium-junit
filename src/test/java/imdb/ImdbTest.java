1dd3d506e6754b6835478e79ce1d30d96ad63e78
jomarnavarro <jomarnavarro@gmail.com>
2020-02-15 11:27:14 -0600

    first commit

---
+++ src/test/java/imdb/ImdbTest.java
@@ -0,0 +1,40 @@
+package imdb;
+
+import org.junit.Test;
+
+public class ImdbTest extends ParentTest{
+	
+	@Test
+	public void testSearchMovie() {
+		navigateToPage("http://imdb.com");
+		validatePage();
+		searchMovie("The Avengers");
+	}
+	
+	
+	@Test
+	public void testPlayTrailer() throws InterruptedException {
+		navigateToPage("http://imdb.com");
+		validatePage();
+		searchMovie("The Avengers");
+		validateMovieExists("The Avengers");
+		selectMovie("The Avengers", "2012");
+		Thread.sleep(5000);
+		validateCorrectMovie();
+		playTrailer();
+	}
+
+	@Test
+	public void testValidateMovieStars() {
+		navigateToPage("http://imdb.com");
+		validatePage();
+		searchMovie("The Avengers");
+		validateMovieExists("The Avengers");
+		selectMovie("The Avengers", "2012");
+		validateCorrectMovie();
+		validateMovieStars();
+	}
+
+	
+
+}
---
+++ src/test/java/imdb/ParentTest.java
@@ -0,0 +1,89 @@
+package imdb;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertTrue;
+
+public class ParentTest {
+	
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+	
+	@Before
+	public void setUp() {
+		
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, 5);
+		
+		
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
+		
+	}
+	protected void validateMovieExists(String movieName) {
+		// TODO Auto-generated method stub
+		WebElement resultado = driver.findElement(By.linkText(movieName));
+		wait.until(ExpectedConditions.visibilityOf(resultado));
+		assertTrue(resultado.isDisplayed());
+	}
+	protected void searchMovie(String movieName) {
+
+		//encontrar el campo de busqueda name= "q"
+		WebElement campoBusqueda = driver.findElement(By.name("q"));
+		campoBusqueda.sendKeys(movieName);
+		//encontrar el boton de busqueda id="navbar-submit-button
+		WebElement botonBusqueda = driver.findElement(By.id("navbar-submit-button"));
+		botonBusqueda.click();
+	}
+	//Econtrar el campo busqueda
+
+	protected void validatePage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void navigateToPage(String url) {
+		// TODO Auto-generated method stub
+		driver.navigate().to(url);
+	}
+	
+	protected void playTrailer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void validateCorrectMovie() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void selectMovie(String movieName, String movieYear) {
+		// encontramos un elemento cuyo link diga 'movieName' y cuyo anio coincida con movieYear
+		String xpathResultado = "//td[contains(., '" + movieName + " (" + movieYear+ ")')]/a";
+		WebElement peliculaCorrecta = driver.findElement(By.xpath(xpathResultado));
+		peliculaCorrecta.click();
+
+		// TODO Auto-generated method stub
+		
+	}
+	
+	protected void validateMovieStars() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
